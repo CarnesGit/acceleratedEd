d59d8d246700d381c6b2e9c78aed56221a0fabb9
Rick <rick.carnes@gmail.com>
2020-10-28 23:51:11 -0400

    fixed page2.js logic for page completion

--- Javascript/page2.js
+++ Javascript/page2.js
@@ -1,69 +1,78 @@
 $(document).ready(function () {
+  const finished = ()=>{
+    if (counter === 6){
+        $('.confetti').show()
+        $('#done').show();
+        $('html, body').scrollTop($(document).height());
+    }
+}
     $('#done').hide()
     $('.confetti').hide()
     let counter = 0
-    $('#1a').on('change', function() {
+    $('#1a').on('click', function() {
       $('#1ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#1b').on('change', function() {
+    $('#1b').on('click', function() {
       $('#1a').attr("disabled",true);
       $('#1ans').text("Good job!");
-      $('html, body').scrollTop($(document).height());
       counter++;
+      finished();
+      $('html, body').scrollTop($(document).height());
     });
-    $('#2a').on('change', function() {
+    $('#2a').on('click', function() {
       $('#2b').attr("disabled",true);
       $('#2ans').text("Good Job!");
-      $('html, body').scrollTop($(document).height());
       counter++;
+      finished();
+      $('html, body').scrollTop($(document).height());
     });
-    $('#2b').on('change', function() {
+    $('#2b').on('click', function() {
       $('#2ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#3a').on('change', function() {
+    $('#3a').on('click', function() {
       $('#3ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#3b').on('change', function() {
+    $('#3b').on('click', function() {
       $('#3a').attr("disabled",true);
       $('#3ans').text("Good job!");
-      $('html, body').scrollTop($(document).height());
       counter++;
+      finished();
+      $('html, body').scrollTop($(document).height());
     });
-    $('#4a').on('change', function() {
+    $('#4a').on('click', function() {
       $('#4b').attr("disabled",true);
       $('#4ans').text("Good Job!");
-      $('html, body').scrollTop($(document).height());
       counter++;
+      finished();
+      $('html, body').scrollTop($(document).height());
     });
-    $('#4b').on('change', function() {
+    $('#4b').on('click', function() {
       $('#4ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#5a').on('change', function() {
+    $('#5a').on('click', function() {
       $('#5ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#5b').on('change', function() {
+    $('#5b').on('click', function() {
       $('#5a').attr("disabled",true);
       $('#5ans').text("Good job!");
-      $('html, body').scrollTop($(document).height());
       counter++;
+      finished();
+      $('html, body').scrollTop($(document).height());
     });
-    $('#6a').on('change', function() {
+    $('#6a').on('click', function() {
       $('#6ans').text("Take another look at the examples in this lesson.");
       $('html, body').scrollTop($(document).height());
     });
-    $('#6b').on('change', function() {
+    $('#6b').on('click', function() {
       $('#6a').attr("disabled",true);
       $('#6ans').text("Good job!");
       counter++;
-      if (counter === 6){
-        $('.confetti').show()
-        $('#done').show();
-      }
+      finished();
       $('html, body').scrollTop($(document).height());
     });
   });
