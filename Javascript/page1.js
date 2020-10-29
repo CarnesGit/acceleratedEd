5cb607de0d308c2fe8e9f66347a50dc4282bff21
Rick <rick.carnes@gmail.com>
2020-10-29 00:12:28 -0400

    stopped auto scroll on carousel page1

--- Javascript/page1.js
+++ Javascript/page1.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
-    $('.confetti').hide()
-    $('#done').hide()
-    $('#yes').on('change', function() {
-      $('.confetti').show()
-      $('#answer').text("Yes. Many organizations could fall under the idea of political organizations or interest groups since they tend to aim for political influence and focus on areas of common interest. However, some are more explicitly political, while others have politics as a goal among many.");
-      $('#doneH3').text("Good Job! Move on to the next page.");
-      $('#done').show();
-      $('html, body').scrollTop($(document).height());
-    });
-    $('#no').on('change', function() {
-      $('#answer').text("Sorry that is incorrect. Many organizations could fall under the idea of political organizations or interest groups since they tend to aim for political influence and focus on areas of common interest. However, some are more explicitly political, while others have politics as a goal among many.");
-      $('#doneH3').text("Move on to the next page.")
-      $('#done').show();
-      $('html, body').scrollTop($(document).height());
-    })
+  $('.carousel').carousel({
+    interval: false,
   });
+  $('.confetti').hide()
+  $('#done').hide()
+  $('#yes').on('change', function() {
+    $('.confetti').show()
+    $('#answer').text("Yes. Many organizations could fall under the idea of political organizations or interest groups since they tend to aim for political influence and focus on areas of common interest. However, some are more explicitly political, while others have politics as a goal among many.");
+    $('#doneH3').text("Good Job! Move on to the next page.");
+    $('#done').show();
+    $('html, body').scrollTop($(document).height());
+  });
+  $('#no').on('change', function() {
+    $('#answer').text("Sorry that is incorrect. Many organizations could fall under the idea of political organizations or interest groups since they tend to aim for political influence and focus on areas of common interest. However, some are more explicitly political, while others have politics as a goal among many.");
+    $('#doneH3').text("Move on to the next page.")
+    $('#done').show();
+    $('html, body').scrollTop($(document).height());
+  })
+});
